f1d618f295c203bc4fd72287e62bf810079186e9
Aaron Richter <aaron.richter@modernizingmedicine.com>
2018-02-04 10:58:31 -0500

    jupyter notebook and medicare data prep

--- experiments/jupyter.py
+++ experiments/jupyter.py
@@ -1,29 +1,20 @@
 import boto3
 import logging
 import argparse
-import subprocess
 import os
-import uuid
-from datetime import datetime
-from experiments.helpers import json_dump_s3
 
 logger = logging.getLogger('root')
+logger.setLevel(logging.INFO)
 logFormatter = logging.Formatter('%(asctime)s %(process)d [%(funcName)s] %(levelname)s: %(message)s')
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(logFormatter)
-console_handler.setLevel(logging.INFO)
 logger.addHandler(console_handler)
-
-REQUIREMENTS = ['boto3', 'scikit-learn==0.19.0', 'scipy==1.0.0', 'feather-format']
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="Path to Python scripts")
-    parser.add_argument("script", help="Name of the script to execute (not including .py)")
-    parser.add_argument("config", help="Name of the config module (not including .py)")
     parser.add_argument("pem", help="Name of the pem file.")
-    parser.add_argument("s3", help="S3 bucket name")
+    parser.add_argument("--port", help="Local port to map jupyter notebook to", default='8888')
     parser.add_argument("--instance-type", default="m4.large")
     parser.add_argument("--bid-price", help="Max bid price for spot instance. If null, will use on-demand.")
 
@@ -31,22 +22,16 @@ def parse_args():
 
 
 args = parse_args()
-path = args.path
-script = args.script
-config = args.config
 pem_file = args.pem
 key_name = os.path.basename(pem_file).split('.')[0]
-s3_bucket = args.s3
+port = args.port
 instance_type = args.instance_type
 bid_price = args.bid_price
 
 ec2 = boto3.client('ec2')
 
-experiment_id = uuid.uuid4()
-experiment_date = datetime.now()
-logger.warn('Experiment ID: {}'.format(experiment_id))
 
-logger.warn('Launching instance')
+logger.info('Launching instance')
 tags = [{
     'ResourceType': 'instance',
     'Tags': [
@@ -91,64 +76,16 @@ else:
     )
 
 instance_id = create['Instances'][0]['InstanceId']
-logger.warn('Instance ID: {}'.format(instance_id))
+logger.info('Instance ID: {}'.format(instance_id))
 
 waiter = ec2.get_waiter('instance_status_ok')
 waiter.wait(InstanceIds=[instance_id])
 
 instance = ec2.describe_instances(InstanceIds=[instance_id])
 public_dns = instance['Reservations'][0]['Instances'][0]['PublicDnsName']
-logger.warn('To view output:  ssh -i {pem} ec2-user@{host} "tail -100f log.txt"'.format(pem=pem_file, host=public_dns))
+logger.info('Public DNS name: {}'.format(public_dns))
 
-exp_data = {'experiment_id': '{}'.format(experiment_id),
-            'experiment_date': '{}'.format(experiment_date),
-            'instance_id': '{}'.format(instance_id),
-            'instance_type': '{}'.format(instance_type),
-            'public_dns': '{}'.format(public_dns),
-            'terminate': 'aws ec2 terminate-instances --instance-ids {}'.format(instance_id),
-            'tail_log': 'ssh -i {pem} ec2-user@{host} "tail -100f log.txt"'.format(pem=pem_file, host=public_dns)
-            }
-json_dump_s3(exp_data, s3_bucket, 'experiments/{}_{}.json'.format(experiment_date.strftime('%Y-%m-%d_%H:%M:%S'), experiment_id))
-
-commands = """
-source activate tensorflow_p36
-
-instanceid={instance_id}
-
-echo 'Installing dependencies'
-conda install {dep} -c conda-forge
-
-echo 'Running script'
-python {script}.py {config} --experiment-id {exp_id} --bucket {bucket}
-
-echo 'Save logs'
-logdate=$(date '+%Y-%m-%d_%H:%M:%S_')
-logfile="$logdate$instanceid"
-aws s3 cp log.txt "s3://{bucket}/logs/$logfile.txt"
-aws s3 cp resources.csv "s3://{bucket}/logs/$logfile-resources.csv"
-
-# Terminate instance
-sudo shutdown -h now
-""".format(instance_id=instance_id,
-           dep=' '.join(REQUIREMENTS),
-           path=path,
-           script=script,
-           config=config,
-           exp_id=experiment_id,
-           bucket=s3_bucket)
-with open("/tmp/command.sh", "w") as text_file:
-    text_file.write(commands)
-
-# copy scripts to instance
-ssh_command = "scp -r -i {} -o StrictHostKeyChecking=no {}/* ec2-user@{}:/home/ec2-user".format(pem_file, path, public_dns)
-subprocess.check_call(ssh_command, shell=True)
-ssh_command = "scp -r -i {} -o StrictHostKeyChecking=no /tmp/command.sh ec2-user@{}:/home/ec2-user".format(pem_file, public_dns)
-subprocess.check_call(ssh_command, shell=True)
-
-# launch experiment
-ssh_command = 'ssh -i {pem} -o StrictHostKeyChecking=no ec2-user@{host} "nohup bash command.sh &> log.txt &"'.format(pem=pem_file, host=public_dns)
-subprocess.Popen(ssh_command, shell=True)
-
-# launch resource logging
-ssh_command = 'ssh -i {pem} -o StrictHostKeyChecking=no ec2-user@{host} "nohup bash log_resources.sh &> resources.csv &"'.format(pem=pem_file, host=public_dns)
-subprocess.Popen(ssh_command, shell=True)
+jupyter_command = 'ssh -i {pem} -L 8000:localhost:{port} ec2-user@{host}'.format(pem=pem_file, port=port, host=public_dns)
+logger.info('SSH command: {}'.format(jupyter_command))
+logger.info('Jupyter commands: jupyter notebook --no-browser --port=8888')
+logger.info("Don't forget to copy notebooks and terminate instance when you're done!!")
